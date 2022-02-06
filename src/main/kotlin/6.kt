daf5f78d57fdd79604e6ae714bf589a1fd958ecd
zahhard <za.darardoust@gmail.com>
2022-02-07 01:16:20 +0330

    version1 of question 6

--- src/main/kotlin/6.kt
+++ src/main/kotlin/6.kt
@@ -1,25 +1,57 @@
-fun main(){
+fun main() {
 
-
+    var count1 = 0
+    var count2 = 0
+    var count3 = 0
+    var count4 = 0
     var n = 5;
-    println("Your input is: " + n);
+    println("Your input is: $n")
+    count1++
+
     for (i in 0 until n) {
-        println("Your output is " + i);
+        println("Your output is $i")
+        count2++
     }
-    for (i in 0..n) {
-        for(j in 0..n) {
-        println("Your output is " + i + " and " + j);
+    for (i in 0 until  n) {
+        for (j in 0 until  n) {
+            println("Your output is $i and $j")
+            count3++
+        }
     }
+
+    for (i in 0 until  factorial(n)) {
+        println("Your output is $i")
+        count4++
     }
-    for  (i in 0.. factorial(n)){
-        println("Your output is " + i);
-    }
+
+    witchOne(count1)
+    witchOne(count2)
+    witchOne(count3)
+    witchOne(count4)
 }
 
-fun factorial(n: Int): Int{
+fun factorial(n: Int): Int {
     var fact = 2
-    for (i in 3 until n){
+    for (i in 3..n) {
         fact *= i
     }
     return fact
+}
+
+fun witchOne(count: Int, n: Int = 5){
+    if (count == n) {
+        println("O(n)    *   $count")
+    }
+    else if (count == factorial(n)) {
+        println("O(n!)   *   $count")
+    }
+    else if (count == n * n) {
+        println("O(n^2)  *   $count")
+    }
+    else{
+        println("O(1)    *   $count")
+    }
+
+
+
 }
