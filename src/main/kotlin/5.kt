b301bceb397b12cb31b1150720ee220e3056ab63
zahhard <za.darardoust@gmail.com>
2022-02-09 22:18:42 +0330

    i remove list class

--- src/main/kotlin/5.kt
+++ src/main/kotlin/5.kt
@@ -1,15 +1,14 @@
-class List<T> (var array: ArrayList<T>)
-
 fun main(){
-    val list1 = List(arrayListOf(1, 2, 3, 4, 5))
+    val list1 = listOf(1, 2, 3, 4, 5)
     list1.slice(2)
 
-    val list2 = List(arrayListOf("a","b","c","d","e","f","g"))
+    val list2 = listOf("a","b","c","d","e","f","g")
     list2.slice(3)
 }
 
 fun <T>List<T>.slice (n: Int) {
-    println(array.take(n + 1))
-    println(array.takeLast( array.size - n - 1))
+
+    println(this.take(n + 1))
+    println(this.takeLast( this.size - n - 1))
 }
 
