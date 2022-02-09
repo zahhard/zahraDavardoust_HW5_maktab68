83bfec5b0604983ac800dd64c9e089789585537d
zahhard <za.darardoust@gmail.com>
2022-02-09 22:41:33 +0330

    i change all of it because hashCode was a silly way here and now it is not true too

--- src/main/kotlin/3.kt
+++ src/main/kotlin/3.kt
@@ -1,17 +1,36 @@
-class BiggerOne<T>(var num1:T, var num2: T){
+class BiggerOne<T>(var num1: T, var num2: T) {
 
-    fun compare(): T{
-        if (num1.hashCode() < num2.hashCode())
-            return num2
-        else
-            return num1
-    }
-
-    fun <T: Comparable<T>> sort(list: List<T>): List<T> {
-        return list.sorted()
+    fun compare(): T {
+        return num1
     }
 }
-fun main(){
-    val biggerOne = BiggerOne("sss", 120)
-    println(biggerOne.compare())
+
+
+fun main() {
+    //val biggerOne = BiggerOne("1", 120)
+    // println(biggerOne.compare())
+    println()
+
+    val o1: Any = 2
+    val o2: Any = 4
+    if (o1 is Comparable<*> && o1::class == o2::class) {
+        @Suppress("UNCHECKED_CAST")
+        if (o1 as Comparable<Any> < o2) {
+            println("it's less")
+        }
+    }
+}
+
+class Generic<T : Any>(val klass: Class<T>) {
+    companion object {
+        inline operator fun <reified T : Any>invoke() = Generic(T::class.java)
+    }
+
+    fun checkType(t: Any) {
+        when {
+            klass.isAssignableFrom(t.javaClass) -> true
+            else -> false
+        }
+
+    }
 }
