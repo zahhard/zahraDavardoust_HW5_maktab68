1d800c7280ab46a98b4dcec2f949b34c898119f0
zahhard <za.darardoust@gmail.com>
2022-02-09 20:46:38 +0330

    finaly

--- src/main/kotlin/5.kt
+++ src/main/kotlin/5.kt
@@ -1,5 +1,15 @@
-class List<T> (a: ArrayList<T>)
+class List<T> (var a: ArrayList<T>)
 
-fun List<Any>.pars(n: Int){
+fun main(){
+    val list1 = List(arrayListOf(1, 2, 3, 4, 5))
+    list1.slice(2)
 
+    val list2 = List(arrayListOf("a","b","c","d","e","f","g"))
+    list2.slice(3)
 }
+
+fun <T>List<T>.slice (n: Int) {
+    println(a.take(n + 1))
+    println(a.takeLast( a.size - n - 1))
+}
+
